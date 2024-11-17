b5a31a61838e1054f779c681c70ea1aa91b493ec
UNDERDOG <u21203803@utp.edu.pe>
2024-11-17 02:47:58 -0500

    Controles primera parte

--- js/main.js
+++ js/main.js
@@ -6,9 +6,16 @@ const map = new ol.Map({
     }),
   ],
   view: new ol.View({
-    center: new ol.proj.fromLonLat([-77.026211,-11.883041]),
+    center: new ol.proj.fromLonLat([-77.026211, -11.883041]),
     zoom: 18,
   }),
 });
 
 console.log(map.getView().getProjection());
+
+map.addControl(
+  new ol.control.FullScreen({
+    className: "proy-ol-full-screen",
+    tipLabel: "Pantalla Completa",
+  })
+);
