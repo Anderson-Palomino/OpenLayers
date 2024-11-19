b1b057d286c0e9892be0f5833f6e1258f4059538
UNDERDOG <u21203803@utp.edu.pe>
2024-11-19 10:49:36 -0500

    datos vectoriales

--- js/VistaInicio.js
+++ js/VistaInicio.js
@@ -8,18 +8,25 @@ const map = new ol.Map({
     new ol.layer.Tile({
       // source: new ol.source.OSM(),
       source: new ol.source.XYZ({
-        url:"http://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}",
-        attributions:"Google Inc.",
-        attributionsCollapsible: false
-      })
+        url: "http://mt1.google.com/vt/lyrs=r&x={x}&y={y}&z={z}",
+        attributions: "Google Inc.",
+        attributionsCollapsible: false,
+      }),
     }),
+    // new ol.layer.Vector({
+    //   source: new ol.source.Vector({
+    //     url:"data/countries.json",
+    //     format: new ol.format.GeoJSON()
+    //   })
+    // })
   ],
   view: new ol.View({
     center: new ol.proj.fromLonLat([-77.026211, -11.883041]),
-    zoom: 18,
+    // center:[0,0],
+    zoom: 15,
+    minZoom: 12,
+    maxZoom: 17,
   }),
 });
 
 console.log(map.getView().getProjection());
-
-
